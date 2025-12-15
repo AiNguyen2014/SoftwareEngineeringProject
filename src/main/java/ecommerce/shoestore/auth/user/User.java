a9097a0f27115abbf04fa9a19fa2b973005b8de1
AiNguyen2014 <chungthiainguyen.56228@gmail.com>
2025-12-15 12:33:50 +0700

    Update Cart and CartItem object

--- src/main/java/ecommerce/shoestore/auth/user/User.java
+++ src/main/java/ecommerce/shoestore/auth/user/User.java
@@ -9,39 +9,43 @@ import ecommerce.shoestore.auth.account.Account;
 import ecommerce.shoestore.auth.address.Address;
 
 @Entity
-@Table(name = "users") 
-@Data @NoArgsConstructor @AllArgsConstructor
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
     @Column(nullable = false)
     private String fullname;
-    
+
     @Column(nullable = false, unique = true)
     private String email;
-    
+
     @Column(nullable = false)
     private LocalDate dateOfBirth;
-    
+
     @Enumerated(EnumType.STRING)
     private UserGender gender;
-    
+
     @Column(nullable = false, unique = true)
     private String phone;
-    
+
     @Column(columnDefinition = "TEXT", nullable = true)
     private String avatar;
 
-    @Column(name = "registration_date", updatable = false) 
+    @Column(name = "registrationDate", updatable = false)
     private LocalDateTime registrationDate;
 
     @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "account_id", referencedColumnName = "accountId")
+    @JoinColumn(name = "accountId", referencedColumnName = "accountId")
     private Account account;
 
     @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "address_id", referencedColumnName = "addressId")
+    @JoinColumn(name = "addressId", referencedColumnName = "addressId")
     private Address address;
 
     @PrePersist
